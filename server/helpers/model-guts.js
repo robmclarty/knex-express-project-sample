9f57f3f26a6f180f519b67e6cf589969899f1bd9
slode <rob.m@myplanet.com>
2018-04-03 15:54:59 -0400

    add a bit more explanation to the model-guts module

--- server/helpers/model-guts.js
+++ server/helpers/model-guts.js
@@ -3,6 +3,10 @@
 // The guts of a model that uses Knexjs to store and retrieve data from a
 // database using the provided `knex` instance. Custom functionality can be
 // composed on top of this set of common guts.
+//
+// The idea is that these are the most-used types of functions that most/all
+// "models" will want to have. They can be overriden/modified/extended if
+// needed by composing a new object out of the one returned by this function ;)
 module.exports = ({
   knex = {},
   name = 'name',
